d01321603fa273ac66d551b3a833cd18998262d4
oguzakif <oguztufek@gmail.com>
2021-06-16 17:59:40 +0300

    errorlabel added in customer profile edit

--- src/controller/CustomerProfileEditController.java
+++ src/controller/CustomerProfileEditController.java
@@ -3,9 +3,13 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class CustomerProfileEditController {
+
+    @FXML
+    private Label errorLabel;
 
     @FXML
     private Button cartButton;
