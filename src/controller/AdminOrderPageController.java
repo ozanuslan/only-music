bd7414f55890dca955d9eba17ae4ce4e961d566a
oguzakif <oguztufek@gmail.com>
2021-06-17 19:35:32 +0300

    Admin order page design, conroller

--- src/controller/AdminOrderPageController.java
+++ src/controller/AdminOrderPageController.java
@@ -4,11 +4,24 @@ import helper.Helper;
 import helper.Storage;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
+import model.Administrator;
+import model.Customer;
+import model.Order;
 
-public class AdminOrderPageController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class AdminOrderPageController implements Initializable {
 
     @FXML
     private Button backwardButton;
@@ -29,6 +42,7 @@ public class AdminOrderPageController {
     private GridPane completedOrdersGrid;
 
     Storage storage = Storage.getStorage();
+    ArrayList<Order> orders = new ArrayList<>();
 
     @FXML
     void backwardButtonAction(ActionEvent event) throws Exception {
@@ -40,4 +54,55 @@ public class AdminOrderPageController {
         Helper.logOut(logoutButton);
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        update();
+    }
+
+    public void update(){
+        int rowPending = 0;
+        int rowCompleted = 0;
+        try {
+            int size = orders.size();
+            for(int i = 0;i<size;i++){
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setLocation(getClass().getResource("/GUI/order-block-admin.fxml"));
+
+                AnchorPane anchorPane = fxmlLoader.load();
+
+                OrderBlockAdminController orderBlockAdminController = fxmlLoader.getController();
+                orderBlockAdminController.setOrder(orders.get(i));
+                orderBlockAdminController.setAdminOrderPageController(this);
+                if(orders.get(i).getStatus() == 0){
+                    pendingOrdersGrid.add(anchorPane, 0, rowPending++);
+                    pendingOrdersGrid.setMinWidth(Region.USE_COMPUTED_SIZE);
+                    pendingOrdersGrid.setPrefWidth(Region.USE_COMPUTED_SIZE);
+                    pendingOrdersGrid.setMaxWidth(Region.USE_PREF_SIZE);
+
+                    pendingOrdersGrid.setMinHeight(Region.USE_COMPUTED_SIZE);
+                    pendingOrdersGrid.setPrefHeight(Region.USE_COMPUTED_SIZE);
+                    pendingOrdersGrid.setMaxHeight(Region.USE_PREF_SIZE);
+                }
+                else{
+                    completedOrdersGrid.add(anchorPane, 0, rowCompleted++);
+                    completedOrdersGrid.setMinWidth(Region.USE_COMPUTED_SIZE);
+                    completedOrdersGrid.setPrefWidth(Region.USE_COMPUTED_SIZE);
+                    completedOrdersGrid.setMaxWidth(Region.USE_PREF_SIZE);
+
+                    completedOrdersGrid.setMinHeight(Region.USE_COMPUTED_SIZE);
+                    completedOrdersGrid.setPrefHeight(Region.USE_COMPUTED_SIZE);
+                    completedOrdersGrid.setMaxHeight(Region.USE_PREF_SIZE);
+                }
+                GridPane.setMargin(anchorPane, new Insets(20,60,20,60));
+
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    void deleteGrid(){
+        Helper.clearScreen(pendingOrdersGrid);
+        Helper.clearScreen(completedOrdersGrid);
+    }
 }
--- src/controller/OrderBlockAdminController.java
+++ src/controller/OrderBlockAdminController.java
@@ -1,9 +1,19 @@
 package controller;
 
+import helper.DatabaseConnection;
+import helper.SceneBuilder;
+import helper.Storage;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import model.CartItem;
+import model.Order;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class OrderBlockAdminController {
 
@@ -22,14 +32,58 @@ public class OrderBlockAdminController {
     @FXML
     private Button cancelButton;
 
-    @FXML
-    void cancelButtonAction(ActionEvent event) {
+    SceneBuilder sceneBuilder = SceneBuilder.getSceneBuilder();
+    Storage storage = Storage.getStorage();
 
+
+    AdminOrderPageController adminOrderPageController;
+
+    private Order order;
+    DatabaseConnection connection = new DatabaseConnection();
+    Connection connectDB = connection.getConnection();
+
+    void setOrder(Order order){
+        this.order = order;
+        receiverLabel.setText(order.getCustomer().getName());
+        priceLabel.setText("$"+String.valueOf(order.getTotalPrice()));
+    }
+    void setAdminOrderPageController(AdminOrderPageController adminOrderPageController){
+        this.adminOrderPageController = adminOrderPageController;
     }
 
     @FXML
-    void checkButtonAction(ActionEvent event) {
+    void cancelButtonAction(ActionEvent event) throws SQLException {
+        order.setStatus(2);
+        adminOrderPageController.deleteGrid();
+        adminOrderPageController.update();
+        Statement statement = connectDB.createStatement();
+        String updateStatusQuery = "UPDATE `order` SET `status` = ? WHERE (`orderId` = ?)";
+        PreparedStatement ps1 = connectDB.prepareStatement(updateStatusQuery);
+        ps1.setString(1, "2");
+        ps1.setString(2, Integer.toString(order.getId()));
+        ps1.executeUpdate();
 
+        String updateStockQuery = "UPDATE `item` SET `stock` = ? WHERE (`idItem` = ?)";
+        PreparedStatement ps2 = connectDB.prepareStatement(updateStockQuery);
+        for (CartItem item : order.getItems()) {
+            ps2.setString(1, Integer.toString(item.getItem().getStock() + item.getQuantity()));
+            ps2.setString(2, Integer.toString(item.getItem().getId()));
+            ps2.executeUpdate();
+            item.getItem().increaseStock(item.getQuantity());
+        }
+    }
+
+    @FXML
+    void checkButtonAction(ActionEvent event) throws SQLException {
+        order.setStatus(1);
+        adminOrderPageController.deleteGrid();
+        adminOrderPageController.update();
+        Statement statement = connectDB.createStatement();
+        String updateStatusQuery = "UPDATE `order` SET `status` = ? WHERE (`orderId` = ?)";
+        PreparedStatement ps1 = connectDB.prepareStatement(updateStatusQuery);
+        ps1.setString(1, "1");
+        ps1.setString(2, Integer.toString(order.getId()));
+        ps1.executeUpdate();
     }
 
     @FXML
