6b2678a49b64980e359945309ed88a4c550e41cd
ozanuslan <ozanuslan@hotmail.com>
2021-06-19 18:43:03 +0300

    Item update now triggers DB update too.

--- src/controller/AdminItemPageController.java
+++ src/controller/AdminItemPageController.java
@@ -13,13 +13,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.scene.control.cell.TextFieldTreeTableCell;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.util.Callback;
 import javafx.util.converter.IntegerStringConverter;
 import model.Item;
 
@@ -29,7 +27,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class AdminItemPageController implements Initializable {
@@ -133,9 +130,10 @@ public class AdminItemPageController implements Initializable {
         loadTable(itemList);
     }
 
-    ObservableList getTableList(String filter){
+    ObservableList getTableList(String filter) {
         return FXCollections.observableList(ContentFilter.getFilteredItemList(storage.getItemList(), filter));
     }
+
     @FXML
     void backwardButtonAction(ActionEvent event) throws Exception {
         Helper.goBackward(backwardButton);
@@ -150,14 +148,13 @@ public class AdminItemPageController implements Initializable {
     void addItemButtonAction(ActionEvent event) throws SQLException {
         if(addItemNameInput.getText().isBlank() || addPriceInput.getText().isBlank() || addAttribute1.getText().isBlank() || addAttribute2.getText().isBlank() || addAttribute3.getText().isBlank() || addAttribute4.getText().isBlank() || addStockInput.getText().isBlank() || addItemType.getValue() == null){
             errorLabel.setText("Please fill the empty spaces.");
-        }
-        else{
-            if(inputValidation()){
-                String queryText = "INSERT INTO item"+"(price,name,attribute1,attribute2,attribute3,attribute4,stock,imagePath,type) VALUES "+"(?,?,?,?,?,?,?,?,?)";
+        } else {
+            if (inputValidation()) {
+                String queryText = "INSERT INTO item" + "(price,name,attribute1,attribute2,attribute3,attribute4,stock,imagePath,type) VALUES " + "(?,?,?,?,?,?,?,?,?)";
                 try {
 
-                    Statement st=connectDB.createStatement();
-                    PreparedStatement ps=connectDB.prepareStatement(queryText);
+                    Statement st = connectDB.createStatement();
+                    PreparedStatement ps = connectDB.prepareStatement(queryText);
 
                     ps.setString(1, addPriceInput.getText());
                     ps.setString(2, addItemNameInput.getText());
@@ -172,7 +169,7 @@ public class AdminItemPageController implements Initializable {
                     ps.executeUpdate();
                     errorLabel.setText("Item has successfully created.");
                     imagePathLabel.setText("");
-                }  catch (Exception e3) {
+                } catch (Exception e3) {
                     e3.printStackTrace();
                     errorLabel.setText("Item name must be different.");
                 }
@@ -185,7 +182,7 @@ public class AdminItemPageController implements Initializable {
 
     @FXML
     void typeInputChanged(ActionEvent event) {
-        switch (addItemType.getValue()){
+        switch (addItemType.getValue()) {
             case "Classical Guitar":
                 attribute1Label.setText("Body Material");
                 attribute2Label.setText("Neck Material");
@@ -254,7 +251,6 @@ public class AdminItemPageController implements Initializable {
                 break;
 
 
-
         }
     }
 
@@ -262,22 +258,22 @@ public class AdminItemPageController implements Initializable {
     void changePhotoButtonAction(ActionEvent event) {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open item image");
-        fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")+"\\src\\images"));
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.dir") + "\\src\\images"));
         fileChooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("All Images", "*.jpg;*.png"),
                 new FileChooser.ExtensionFilter("JPG", "*.jpg"),
                 new FileChooser.ExtensionFilter("PNG", "*.png")
         );
-        File file = fileChooser.showOpenDialog((Stage)changePhotoButton.getScene().getWindow());
+        File file = fileChooser.showOpenDialog((Stage) changePhotoButton.getScene().getWindow());
 
-        if(file != null){
-            if(!file.getPath().contains(System.getProperty("user.dir")+"\\src\\images"))
+        if (file != null) {
+            if (!file.getPath().contains(System.getProperty("user.dir") + "\\src\\images"))
                 imagePathLabel.setText("Image must be chosen from images folder.");
-            else{
+            else {
                 imagePathLabel.setText("");
                 int index = file.getPath().indexOf("\\images");
                 String newPath = file.getPath().substring(index, file.getPath().length());
-                newPath = newPath.replace('\\','/');
+                newPath = newPath.replace('\\', '/');
 
                 imagePathInput.setText(newPath);
                 itemImage.setImage(new Image(getClass().getResourceAsStream(newPath)));
@@ -285,17 +281,17 @@ public class AdminItemPageController implements Initializable {
         }
     }
 
-    boolean inputValidation(){
+    boolean inputValidation() {
         System.out.println(addItemType.getValue());
-        if(!Helper.isPositiveNumber(addPriceInput.getText())){
+        if (!Helper.isPositiveNumber(addPriceInput.getText())) {
             errorLabel.setText("Item price must be bigger than zero");
             return false;
         }
-        if(!Helper.isPositiveNumber(addStockInput.getText())){
+        if (!Helper.isPositiveNumber(addStockInput.getText())) {
             errorLabel.setText("Item stock must be bigger than zero");
             return false;
         }
-        if(addItemType.getValue() == null){
+        if (addItemType.getValue() == null) {
             errorLabel.setText("Item type must be selected");
             return false;
         }
@@ -312,11 +308,11 @@ public class AdminItemPageController implements Initializable {
                     errorLabel.setText("Key Number must bigger than zero");
                     return false;
                 }
-                if (!Helper.isPositiveNumber(addAttribute3.getText())){
+                if (!Helper.isPositiveNumber(addAttribute3.getText())) {
                     errorLabel.setText("Width must be bigger than zero");
                     return false;
                 }
-                if(!Helper.isPositiveNumber(addAttribute4.getText())){
+                if (!Helper.isPositiveNumber(addAttribute4.getText())) {
                     errorLabel.setText("Height must be bigger than zero");
                     return false;
                 }
@@ -326,11 +322,11 @@ public class AdminItemPageController implements Initializable {
                     errorLabel.setText("Snare Drum Number must bigger than zero");
                     return false;
                 }
-                if (!Helper.isPositiveNumber(addAttribute2.getText())){
+                if (!Helper.isPositiveNumber(addAttribute2.getText())) {
                     errorLabel.setText("Bass Drum Number must be bigger than zero");
                     return false;
                 }
-                if(!Helper.isPositiveNumber(addAttribute3.getText())){
+                if (!Helper.isPositiveNumber(addAttribute3.getText())) {
                     errorLabel.setText("Tom Number must be bigger than zero");
                     return false;
                 }
@@ -340,71 +336,71 @@ public class AdminItemPageController implements Initializable {
                     errorLabel.setText("Snare Drum Number must bigger than zero");
                     return false;
                 }
-                if (!Helper.isPositiveNumber(addAttribute2.getText())){
+                if (!Helper.isPositiveNumber(addAttribute2.getText())) {
                     errorLabel.setText("Bass Drum Number must be bigger than zero");
                     return false;
                 }
-                if(!Helper.isPositiveNumber(addAttribute3.getText())){
+                if (!Helper.isPositiveNumber(addAttribute3.getText())) {
                     errorLabel.setText("Tom Number must be bigger than zero");
                     return false;
                 }
                 break;
             case "Violin":
-                if (!Helper.isPositiveNumber(addAttribute3.getText())){
+                if (!Helper.isPositiveNumber(addAttribute3.getText())) {
                     errorLabel.setText("Height must be bigger than zero");
                     return false;
                 }
-                if(!Helper.isPositiveNumber(addAttribute4.getText())){
+                if (!Helper.isPositiveNumber(addAttribute4.getText())) {
                     errorLabel.setText("String Thickness must be bigger than zero");
                     return false;
                 }
                 break;
             case "Cello":
-                if (!Helper.isPositiveNumber(addAttribute3.getText())){
+                if (!Helper.isPositiveNumber(addAttribute3.getText())) {
                     errorLabel.setText("Height must be bigger than zero");
                     return false;
                 }
-                if(!Helper.isPositiveNumber(addAttribute4.getText())){
+                if (!Helper.isPositiveNumber(addAttribute4.getText())) {
                     errorLabel.setText("String Count must be bigger than zero");
                     return false;
                 }
                 break;
             case "Amp":
-                if(!Helper.isPositiveNumber(addAttribute2.getText())){
+                if (!Helper.isPositiveNumber(addAttribute2.getText())) {
                     errorLabel.setText("Power must be bigger than zero");
                     return false;
                 }
-                if(!Helper.isPositiveNumber(addAttribute4.getText())){
+                if (!Helper.isPositiveNumber(addAttribute4.getText())) {
                     errorLabel.setText("Channel Number must be bigger than zero");
                     return false;
                 }
                 break;
             case "Clarinet":
-                if(!Helper.isPositiveNumber(addAttribute2.getText())){
+                if (!Helper.isPositiveNumber(addAttribute2.getText())) {
                     errorLabel.setText("Bore Width must be bigger than zero");
                     return false;
                 }
-                if(!Helper.isPositiveNumber(addAttribute3.getText())){
+                if (!Helper.isPositiveNumber(addAttribute3.getText())) {
                     errorLabel.setText("Key Number must be bigger than zero");
                     return false;
                 }
                 break;
             case "Trombone":
-                if(!Helper.isPositiveNumber(addAttribute2.getText())){
+                if (!Helper.isPositiveNumber(addAttribute2.getText())) {
                     errorLabel.setText("Bore Width must be bigger than zero");
                     return false;
                 }
-                if(!Helper.isPositiveNumber(addAttribute3.getText())){
+                if (!Helper.isPositiveNumber(addAttribute3.getText())) {
                     errorLabel.setText("Valve Number must be bigger than zero");
                     return false;
                 }
                 break;
             case "Trumpet":
-                if(!Helper.isPositiveNumber(addAttribute2.getText())){
+                if (!Helper.isPositiveNumber(addAttribute2.getText())) {
                     errorLabel.setText("Bore Width must be bigger than zero");
                     return false;
                 }
-                if(!Helper.isPositiveNumber(addAttribute4.getText())){
+                if (!Helper.isPositiveNumber(addAttribute4.getText())) {
                     errorLabel.setText("Piston Valve must be bigger than zero");
                     return false;
                 }
@@ -418,24 +414,30 @@ public class AdminItemPageController implements Initializable {
     //********************ITEM EDIT PAGE************************
 
 
-    public void loadTable(ObservableList<Item> items){
+    public void loadTable(ObservableList<Item> items) {
         tableView.setEditable(true);
 
-        itemName.setCellValueFactory(new PropertyValueFactory<Item,String>("name"));
-        itemId.setCellValueFactory(new PropertyValueFactory<Item,Integer>("id"));
-        itemPrice.setCellValueFactory(new PropertyValueFactory<Item,Double>("price"));
+        itemName.setCellValueFactory(new PropertyValueFactory<Item, String>("name"));
+        itemId.setCellValueFactory(new PropertyValueFactory<Item, Integer>("id"));
+        itemPrice.setCellValueFactory(new PropertyValueFactory<Item, Double>("price"));
         itemStock.setCellValueFactory((new PropertyValueFactory<Item, Integer>("stock")));
         itemStock.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
         itemStock.setOnEditCommit(
                 new EventHandler<TableColumn.CellEditEvent<Item, Integer>>() {
                     @Override
                     public void handle(TableColumn.CellEditEvent<Item, Integer> t) {
-                        if(t.getNewValue() >= 0){
-                            ((Item) t.getTableView().getItems().get(
-                                    t.getTablePosition().getRow())
-                            ).setStock(t.getNewValue());
-                        }
-                        else{
+                        if (t.getNewValue() >= 0) {
+                            Item item = ((Item) t.getTableView().getItems().get(t.getTablePosition().getRow()));
+                            item.setStock(t.getNewValue());
+                            String updateQuery = "UPDATE `item` SET `stock` = " + t.getNewValue() + " WHERE idItem = " + item.getId();
+                            try {
+                                Statement statement = connectDB.createStatement();
+                                PreparedStatement ps = connectDB.prepareStatement(updateQuery);
+                                ps.executeUpdate();
+                            } catch (SQLException throwables) {
+                                throwables.printStackTrace();
+                            }
+                        } else {
                             ((Item) t.getTableView().getItems().get(
                                     t.getTablePosition().getRow())
                             ).setStock(t.getOldValue());
